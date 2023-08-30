9cce78ee826f7fb22fdba1339a39ce678e526201
“abdugaffornishonov04” <abdugaffor004n@gmail.com>
2023-08-30 01:01:10 -0800

    Lesson 15 done

---
+++ js/categories.js
@@ -0,0 +1,27 @@
+const ctMainRow = document.querySelector( ".categorii-bottom" );
+
+function ctGetCards( cat ) {
+  const ctMainCard = document.createElement( "div" );
+  ctMainCard.className = "categorii-bottom__card";
+  const ctMainCardImg = document.createElement( "img" );
+  ctMainCardImg.src = `${cat.image}`;
+  ctMainCardImg.alt = `${cat.name}`;
+  const ctMainCardTitle = document.createElement( "h3" );
+  ctMainCardTitle.className = "categorii-bottom__card__title";
+  ctMainCardTitle.innerHTML = `${cat.name}`;
+  const ctFloat = document.createElement( "div" );
+  ctFloat.className = "categorii-float";
+
+  ctMainCard.addEventListener( 'click', () => {
+    window.location.href = 'category.html';
+  } );
+
+
+  ctMainCard.append( ctMainCardImg, ctMainCardTitle, ctFloat )
+  return ctMainCard;
+}
+
+categories.map( ( pr ) => {
+  let card = ctGetCards( pr );
+  ctMainRow.append( card )
+} )
---
+++ js/main.js
@@ -0,0 +1,34 @@
+let tapToSeeNavbar = document.querySelector(".tapToSeeNavbar");
+let resNavbar = document.querySelector(".header-responsive-navbar");
+let hCategoryDropDown = document.querySelector(".header-category-dropdown");
+let hCategoryDropDownContent = document.querySelector(".header-category-dropdown-content");
+let hCategoryDropDownContentRow = document.querySelector(".header-category-dropdown-content-row");
+let resCdDo = document.querySelector(".res-c-d-d-o");
+
+tapToSeeNavbar.addEventListener("click", () => {
+  resNavbar.style.left = "-10px";
+})
+
+hCategoryDropDown.addEventListener("click", () => {
+  hCategoryDropDownContent.classList.toggle("top-eighty")
+})
+
+resCdDo.addEventListener("click", () => {
+  hCategoryDropDownContent.classList.toggle("top-eighty")
+})
+
+////header dropdown and map
+
+function categoryCreateLink (el){
+  const aLink = document.createElement("a");
+  aLink.href = "category.html";
+  aLink.innerHTML = `${el.name}`
+
+  return aLink
+}
+
+categories.map((el) => {
+  let card = categoryCreateLink(el)
+  hCategoryDropDownContentRow.append(card)
+})
+
